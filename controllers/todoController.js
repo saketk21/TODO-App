8676cc70c00a2b5297db9bc15c6c68c9bcdfa96e
Saket <saketpkulkarni@gmail.com>
2018-08-02 11:57:08 +0530

    Handled HTTP POST and DELETE requests

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,16 +1,24 @@
+const bodyParser = require('body-parser');
+
 let data = [{item: "Get Milk"}, {item: "Walk Dog"}, {item: "Kick some coding ass!"}];
+let urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
   app.get('/todo', function(request, response) {
     response.render('todo', {todos: data});
   });
 
-  app.post('/todo', function(request, response) {
-
+  app.post('/todo', urlencodedParser, function(request, response) {
+    data.push(request.body);
+    response.json(data);
   });
 
-  app.delete('/todo', function(request, response) {
-
+  app.delete('/todo/:item', function(request, response) {
+    data = data.filter(function(todo) {
+      return todo.item.replace(/ /g, "-") !== request.params.item;
+    });
+    console.log(data);
+    response.json(data);
   });
 
 };
