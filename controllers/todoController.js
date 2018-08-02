03dc4716359acc782eaf254db6cbb8a4a6940688
Saket <saketpkulkarni@gmail.com>
2018-08-02 11:34:39 +0530

    Added dynamic rendering functionality of Todos

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,8 @@
+let data = [{item: "Get Milk"}, {item: "Walk Dog"}, {item: "Kick some coding ass!"}];
+
 module.exports = function(app) {
   app.get('/todo', function(request, response) {
-    response.render('todo');
+    response.render('todo', {todos: data});
   });
 
   app.post('/todo', function(request, response) {
